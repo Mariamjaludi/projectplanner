ad93c85c36740632ea38fdf0491a98bdb651638a
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-08 20:34:53 +0300

    sign up basic functionality set up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />          
           <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>

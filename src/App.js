dc3376fa1e780a3fb7f06b3c63942c2066207bc6
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-08 18:23:44 +0300

    starting navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import Navbar from './components/layout/Navbar'
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>project planner</h1>
+        <Navbar />
       </div>
     </Router>
   );

881569dc9b7b1102a1900b4b7b3f29caf005526b
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-08 22:24:53 +0300

    thunk set up for async calls

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,6 +1,16 @@
-const initState = {}
+const initState = {
+  projects: [
+    { id: '1', title: 'project 1 title', content: 'this is the content for project 1'},
+    { id: '2', title: 'project 2 title', content: 'this is the content for project 2'},
+    { id: '3', title: 'project 3 title', content: 'this is the content for project 3'}
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log('created project', action.project)
+  }
   return state
 }
 
